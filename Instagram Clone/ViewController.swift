9875cebb758fe44377708ca155e0bbb178e1aa8b
Cengiz Baygın <qrowlite02@icloud.com>
2020-05-24 23:55:16 +0300

    Initial Commit

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by Cengiz Baygın on 24.05.2020.
+//  Copyright © 2020 Cengiz Baygın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
